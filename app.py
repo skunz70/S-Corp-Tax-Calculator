042b86dbc26049a1bf6b3f1f5b27fc772dc761d0
skunz70 <skunz@valhallataxservice.com>
2025-09-09 12:47:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,9 @@ def calculate():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
